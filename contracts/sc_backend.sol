102a3786379a2fdfd846a4798e70d7877c428f8a
multigiachi <giacchy57@live.it>
2021-02-17 18:00:18 +0100

    Lavofo 17feb (test ok)

--- contracts/sc_backend.sol
+++ contracts/sc_backend.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.4.25;
 pragma experimental ABIEncoderV2;
 
 contract sc_backend {
+
     address public backend;
     address public oracle;
     struct Booking {
-        address add;
         uint256 startDate;
         uint256 endDate;
         string city;
@@ -32,23 +32,24 @@ contract sc_backend {
     
     function setBooking(address add, uint256 startDate, uint256 endDate, string city, uint payment) public {
         var booking = bookings[add];
-
-        booking.add = add;
         booking.startDate = startDate;
         booking.endDate = endDate;
         booking.city = city;
         booking.payment = payment;
         
         bookingsAddr.push(add) -1;
-
     }
     
     function getBookingsAll() public view returns (address[]) {
         return bookingsAddr;
     }
     
-    function getLatestBook(address ins) public view returns (address,uint256,uint256,string,uint) {
-        return (bookings[ins].add, bookings[ins].startDate, bookings[ins].endDate, bookings[ins].city, bookings[ins].payment);
+    function getBookingByAdd(address ins) public view returns (uint256,uint256,string,uint) {
+        return (bookings[ins].startDate, bookings[ins].endDate, bookings[ins].city, bookings[ins].payment);
+    }
+
+    function getBookingNumber() public view returns (uint256) {
+        return bookingsAddr.length;
     }
     
     modifier restricted() {
